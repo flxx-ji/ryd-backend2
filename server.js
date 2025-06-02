27274824603f13037b3a72ac9f3c4d4798e0f131
Jean-marc Bastareaud <jean-marcbastareaud@FlxxiBook.local>
2025-06-02 16:49:43 +0200

    changement de cors dans le server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const app = express();
 
 // 🌍 CORS
 app.use(cors({
-  origin: '*',
+  origin: 'https://ryd-custom-motorcycle.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
